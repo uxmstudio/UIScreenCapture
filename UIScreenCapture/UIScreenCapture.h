c8664c2403a903dd8de642f7fcfef283653cbf59
Chris Anderson <hi@chrisanderson.io>
2015-12-28 16:05:10 +0100

    Initial Commit

---
+++ UIScreenCapture/UIScreenCapture.h
@@ -0,0 +1,19 @@
+//
+//  UIScreenCapture.h
+//  UIScreenCapture
+//
+//  Created by Chris Anderson on 12/28/15.
+//  Copyright © 2015 UXM Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIScreenCapture.
+FOUNDATION_EXPORT double UIScreenCaptureVersionNumber;
+
+//! Project version string for UIScreenCapture.
+FOUNDATION_EXPORT const unsigned char UIScreenCaptureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIScreenCapture/PublicHeader.h>
+
+
